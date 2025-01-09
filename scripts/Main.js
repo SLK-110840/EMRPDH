06a278f74c3c6ec54bfacb508a4e523697c7bcf8
SLK-110840 <preeti.joshi@slkgroup.com>
2025-01-09 10:41:27 +0530

    Update Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -123,7 +123,7 @@ define("EMRPDH/scripts/Main", [
 								alert("in else");
            var iUrl1="https://emr-product-datahub-sap-stage.azurewebsites.net/caDetails/"+draggedObjId;
 								alert(iUrl1);
-	widget.body.innerHTML = "<div class='droppableFrame'><img id='dropImage' alt='Drop Here' src='"+dropIconUrl+"'></div><div class='droppedFrame'></div><iframe src='"+iUrl+"' title='description' style='width: 100vw; height: 100vh;'></iframe>";
+	widget.body.innerHTML = "<div class='droppableFrame'><img id='dropImage' alt='Drop Here' src='"+dropIconUrl+"'></div><div class='droppedFrame'></div><iframe src='"+iUrl1+"' title='description' style='width: 100vw; height: 100vh;'></iframe>";
           theDroppedElt.innerHTML = "<iframe srcdoc='<pre>" + iUrl1 + "</pre>' title='description' style='width: 100vw; height: 100vh;'></iframe>"; 
      } 
 						}
