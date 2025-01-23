3e2ece1bbc0c271d3fee059f654323dcbd174e47
SLK-110840 <preeti.joshi@slkgroup.com>
2025-01-23 20:00:20 +0530

    Update Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -53,9 +53,9 @@ define("EMRPDH/scripts/Main", [
 				
 				var droppableContainer = widget.body.querySelector('.droppableContainer');
 				
-				DataDragAndDrop.droppable( theDropElt , {  
+				DataDragAndDrop.droppable( droppableContainer , {  
                 drop : function(data) {
-			
+			droppableContainer.classList.remove("drag-over");
 			alert("before1");
 			 widget.body.innerHTML = "<div class='droppableFrame'><src='"+appUrl+"'></div><div class='droppedFrame'></div><iframe src='' title='description' style='width: 100vw; height: 100vh;'></iframe>";
 			alert("after1");	  
