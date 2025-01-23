5bc44877b14ec79e77f7532722b524d4256c4fab
SLK-110840 <preeti.joshi@slkgroup.com>
2025-01-23 16:49:46 +0530

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const properties = {
     caDetailurl: "https://emr-product-datahub-stage.azurewebsites.net/mcodetail/",	
     devcaurl: "https://emr-product-datahub-stage.azurewebsites.net/mcolist",
-    applicationurl : "https://emr-product-datahub-sap-stage.azurewebsites.net/"
+    applicationurl : "https://emr-product-datahub-sap-stage.azurewebsites.net/calist"
     };
