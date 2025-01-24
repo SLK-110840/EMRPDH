007ebaf22110c1332d84b8998bf6368cbf20c7b0
SLK-110840 <preeti.joshi@slkgroup.com>
2025-01-24 10:08:36 +0530

    Update Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -114,13 +114,13 @@ define("EMRPDH/scripts/Main", [
           
 	var iUrl="https://emr-product-datahub-sap-stage.azurewebsites.net/mcodetail/"+draggedObjId;
 								
-	widget.body.innerHTML = "<div class='droppableFrame'><img id='dropImage' alt='Drop Here' src='"+dropIconUrl+"'></div><div class='droppedFrame'></div><iframe src='"+iUrl+"' title='description' style='width: 100vw; height: 100vh;'></iframe>";
+	widget.body.innerHTML = "<div class='droppableFrame'></div><div class='droppedFrame'></div><iframe src='"+iUrl+"' title='description' style='width: 100vw; height: 100vh;'></iframe>";
           theDroppedElt.innerHTML = "<iframe srcdoc='<pre>" + iUrl + "</pre>' title='description' style='width: 100vw; height: 100vh;'></iframe>";
      } else {
 								
            var iUrl1=" https://emr-product-datahub-sap-stage.azurewebsites.net/caDetails/"+draggedObjId;
 							
-	widget.body.innerHTML = "<div class='droppableFrame'><img id='dropImage' alt='Drop Here' src='"+dropIconUrl+"'></div><div class='droppedFrame'></div><iframe src='"+iUrl1+"' title='description' style='width: 100vw; height: 100vh;'></iframe>";
+	widget.body.innerHTML = "<div class='droppableFrame'></div><div class='droppedFrame'></div><iframe src='"+iUrl1+"' title='description' style='width: 100vw; height: 100vh;'></iframe>";
           theDroppedElt.innerHTML = "<iframe srcdoc='<pre>" + iUrl1 + "</pre>' title='description' style='width: 100vw; height: 100vh;'></iframe>"; 
      } 
 						}
